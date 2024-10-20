a2cdd2fed2e760c826c455f6c0ae592fa2b628ff
Bui Van Tuan Kiet <tuankiet2640@gmail.com>
2024-10-20 13:09:06 +0700

    finish setting up communications for config user and service registry

--- service-registry/src/main/java/com/example/serviceregistry/ServiceRegistryApplication.java
+++ service-registry/src/main/java/com/example/serviceregistry/ServiceRegistryApplication.java
@@ -2,8 +2,9 @@ package com.example.serviceregistry;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
-@SpringBootApplication
+@SpringBootApplication @EnableEurekaServer
 public class ServiceRegistryApplication {
 
     public static void main(String[] args) {
