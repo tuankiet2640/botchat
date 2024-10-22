d223b2ca7cecf82c904451a2bda9dc8e999b2f57
Bui Van Tuan Kiet <tuankiet2640@gmail.com>
2024-10-22 23:37:20 +0700

    fix eureka port ==> caused by service name mismatch

--- auth-service/src/main/java/com/example/authservice/AuthServiceApplication.java
+++ auth-service/src/main/java/com/example/authservice/AuthServiceApplication.java
@@ -2,8 +2,9 @@ package com.example.authservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@SpringBootApplication
+@SpringBootApplication @EnableDiscoveryClient
 public class AuthServiceApplication {
 
     public static void main(String[] args) {
--- service-registry/src/main/java/com/example/serviceregistry/ServiceRegistryApplication.java
+++ service-registry/src/main/java/com/example/serviceregistry/ServiceRegistryApplication.java
@@ -2,13 +2,15 @@ package com.example.serviceregistry;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.netty.NettyReactiveWebServerFactory;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.annotation.Bean;
 
-@SpringBootApplication @EnableEurekaServer
+@SpringBootApplication
+@EnableEurekaServer
 public class ServiceRegistryApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ServiceRegistryApplication.class, args);
     }
-
 }
